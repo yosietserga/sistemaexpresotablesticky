a0941f640c0a453d8fa5456ca1b31885161fab95
Yosiet Serga <yosietserga@gmail.com>
2021-08-02 11:46:46 -0400

    Update theadSticky.js

--- theadSticky.js
+++ theadSticky.js
@@ -1,15 +1,36 @@
-let firstRun = false;
-window.addEventListener('scroll', function(){
-	setTDWidth();
+let tableWidth = 0;
+let theadLeft  = 0;
+let tdDimensions = [];
+let firstRun = true;
+
+let it = setInterval(function(){
     if ( shouldSticky() ) {
-    	if (!firstRun) {
-    		firstRun = true;
-    		getTDWidth();
-    	}
+        clearInterval( it );
+        document.querySelector('.mdc-data-table').addEventListener('scroll',function(){
+            let toLeft = document.querySelector('.mdc-data-table').scrollLeft;
+            let left   = Math.round( parseFloat(theadLeft - toLeft), 2 );
+            
+            document.querySelector('.vuetable thead').style.left = left +'px';
+            console.log([toLeft, document.querySelector('.vuetable thead').style.left]);
+        });
+    }
+}, 100);
+
+window.addEventListener('scroll', function(){
+    tableWidth = document.querySelectorAll('.vuetable')[0].offsetWidth;
+    if ( shouldSticky() ) {
+        if (firstRun) {
+            firstRun  = false;
+            getTDWidth();
+        }
         document.querySelector('.vuetable thead').classList.add('fixedThead');
+        if (firstRun) {
+            theadLeft = document.querySelector('.vuetable thead').offsetLeft;
+        }
     } else {
         document.querySelector('.vuetable thead').classList.remove('fixedThead');
     }
+    setTDWidth();
 });
 
 window.addEventListener('resize', function(){
@@ -18,30 +39,29 @@ window.addEventListener('resize', function(){
 
 
 function shouldRunSticky() {
-	try {
-    	return Array.from( document.querySelectorAll('.vuetable tbody tr') ).length > 10;
-	} catch(error) {
-		console.log( error );
-	}
+    try {
+        return Array.from( document.querySelectorAll('.vuetable tbody tr') ).length > 10;
+    } catch(error) {
+        console.log( error );
+    }
 }
 
 function shouldSticky() {
-	try {
-		if (shouldRunSticky())
-    		return document.querySelector('.vuetable').getBoundingClientRect().top - getNavHeight() <= 0;
-    	else return false;
-	} catch(error) {
-		console.log( error );
-	}
+    try {
+        if (shouldRunSticky())
+            return document.querySelector('.vuetable').getBoundingClientRect().top - getNavHeight() <= 0;
+        else return false;
+    } catch(error) {
+        console.log( error );
+    }
 }
 
 function getNavHeight() {
     return document.querySelector('.v-toolbar.v-toolbar--fixed').offsetHeight;
 }
 
-let tdDimensions = [];
 function getTDWidth() {
-	let __dinmensions = [];
+    let __dinmensions = [];
     let tr = document.querySelectorAll('.vuetable tbody tr')[0];
     Array.from( tr.querySelectorAll('td') ).map(td => {
         __dinmensions.push({
@@ -53,7 +73,14 @@ function getTDWidth() {
 }
 
 function setTDWidth() {
+    document.querySelectorAll('.vuetable thead')[0].style.width = tableWidth +'px';
+    
     Array.from( document.querySelectorAll('.vuetable thead th') ).map((td, i) => {
         td.style.width = tdDimensions[i]?.width +'px';
+    });
+    
+    Array.from( document.querySelectorAll('.vuetable tbody tr')[0].querySelectorAll('td') ).map((td, i) => {
+        td.style.width = tdDimensions[i]?.width +'px';
+        td.style.minWidth = tdDimensions[i]?.width +'px';
     });
 }
